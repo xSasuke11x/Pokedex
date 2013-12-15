94adac1164d9e76388f72a98414fafaaf199332b
kennedy <kennedy.tran@mymail.champlain.edu>
2013-12-14 20:12:04 -0500

    Reads basic data from a set array.

--- Pokedex/gen/edu/champlain/pokedex/R.java
+++ Pokedex/gen/edu/champlain/pokedex/R.java
@@ -8,6 +8,9 @@
 package edu.champlain.pokedex;
 
 public final class R {
+    public static final class array {
+        public static final int pokedexArray=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,18 +27,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090004;
+        public static final int spinnerList=0x7f090002;
+        public static final int submitButton=0x7f090001;
+        public static final int textViewAnswer=0x7f090003;
+        public static final int textViewEvolve=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int evolve=0x7f050004;
+        public static final int selectPrompt=0x7f050002;
+        public static final int submit=0x7f050003;
     }
     public static final class style {
         /** 
@@ -59,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Pokedex/src/edu/champlain/pokedex/MainActivity.java
+++ Pokedex/src/edu/champlain/pokedex/MainActivity.java
@@ -3,14 +3,65 @@ package edu.champlain.pokedex;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView valueTextView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		valueTextView = (TextView) findViewById(R.id.textViewAnswer);
+        Spinner s = (Spinner)findViewById(R.id.spinnerList);
+        final MyData items[] = new MyData[3];
+        items[0] = new MyData( "Chespin","Basic" );
+        items[1] = new MyData( "Quilladin","Evolve Chespin at Lv. 16" );
+        items[2] = new MyData( "Chesnaught","Evolve Quilladin at Lv. 36" );
+        ArrayAdapter<MyData> adapter = new ArrayAdapter<MyData>(this,
+        		android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    MyData d = items[position];
+                    valueTextView.setText( d.getValue() );
+                }
+
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            }
+        );
 	}
+	
+	class MyData {
+		
+        String spinnerText;
+        String value;
+		
+        public MyData( String spinnerText, String value ) {
+            this.spinnerText = spinnerText;
+            this.value = value;
+        }
+
+        public String getSpinnerText() {
+            return spinnerText;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public String toString() {
+            return spinnerText;
+        }
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
