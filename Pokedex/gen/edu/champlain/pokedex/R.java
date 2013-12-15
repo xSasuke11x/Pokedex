dd59d0c95e52e497ef6014f41ced32cf0d56cc4c
kennedy <kennedy.tran@mymail.champlain.edu>
2013-12-15 17:54:05 -0500

    All of the core functions are finished. App now loads in a file and will tell you how to evolve the Pokemon once you select a Pokemon. Need to change UI next. Submit button is useless.

--- Pokedex/gen/edu/champlain/pokedex/R.java
+++ Pokedex/gen/edu/champlain/pokedex/R.java
@@ -9,7 +9,8 @@ package edu.champlain.pokedex;
 
 public final class R {
     public static final class array {
-        public static final int pokedexArray=0x7f060000;
+        public static final int pokedexNames=0x7f060000;
+        public static final int pokedexValues=0x7f060001;
     }
     public static final class attr {
     }
--- Pokedex/src/edu/champlain/pokedex/MainActivity.java
+++ Pokedex/src/edu/champlain/pokedex/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.champlain.pokedex;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,40 +18,62 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	TextView valueTextView;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		valueTextView = (TextView) findViewById(R.id.textViewAnswer);
+        private static final String TAG = null;
+		TextView valueTextView;
+        
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                
+                valueTextView = (TextView) findViewById(R.id.textViewAnswer);
         Spinner s = (Spinner)findViewById(R.id.spinnerList);
-        final MyData items[] = new MyData[3];
-        items[0] = new MyData( "Chespin","Basic" );
-        items[1] = new MyData( "Quilladin","Evolve Chespin at Lv. 16" );
-        items[2] = new MyData( "Chesnaught","Evolve Quilladin at Lv. 36" );
-        ArrayAdapter<MyData> adapter = new ArrayAdapter<MyData>(this,
-        		android.R.layout.simple_spinner_item, items);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-        	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    MyData d = items[position];
-                    valueTextView.setText( d.getValue() );
-                }
+        
+        AssetManager am = this.getAssets();
+        try {
+        	InputStream is = am.open("pokedexfile.txt");
+        	BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        	String line, line2;
+        	final MyData items[] = new MyData[69];
+        	int i = 0;
+        	while (((line = in.readLine()) != null) && ((line2 = in.readLine()) != null)) {
+        		Log.d(TAG, line.toString());
+        		items[i] = new MyData (line, line2);
+        		i++;
+        	}
+        	ArrayAdapter<MyData> adapter = new ArrayAdapter<MyData>(this,
+                    android.R.layout.simple_spinner_item, items);
+		    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		    s.setAdapter(adapter);
+		    s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+	            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+	                MyData d = items[position];
+	                valueTextView.setText( d.getValue() );
+	            }
 
-                public void onNothingSelected(AdapterView<?> parent) {
-                }
-            }
-        );
-	}
-	
-	class MyData {
-		
+	            public void onNothingSelected(AdapterView<?> parent) {
+	            }
+		    });
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        
+        /*int i;
+        for (i = 0; i < 3; i++) {
+        	items[i] = new MyData(tmp1, tmp2);
+        }*/
+        
+        /*items[0] = new MyData( "Chespin","Basic" );
+        items[1] = new MyData( "Quilladin","Evolve Chespin at Lv. 16" );
+        items[2] = new MyData( "Chesnaught","Evolve Quilladin at Lv. 36" );*/
+        
+        }
+        
+        class MyData {
+                
         String spinnerText;
         String value;
-		
+                
         public MyData( String spinnerText, String value ) {
             this.spinnerText = spinnerText;
             this.value = value;
@@ -63,11 +92,11 @@ public class MainActivity extends Activity {
         }
     }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.main, menu);
+                return true;
+        }
 
 }
